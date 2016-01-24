d342751dec19002c5b65e18477fea9cab77e0b28
Derek Haynes <derek.haynes@gmail.com>
2016-01-23 21:15:18 -0700

    setting up unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,7 @@
 worker_processes 50
 timeout 15
 preload_app true
+listen 8080
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
@@ -16,7 +17,6 @@ after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
-  puts "after fork"
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
