4699041a4e6abe535dfe86e0ce3f192777ecc162
Derek Haynes <derek.haynes@gmail.com>
2016-01-27 11:29:00 -0700

    bit less

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
 
   def do_stuff
     sleep 2 if rand(100000) <= 434 # 0.43%
-    City::connection.execute "select pg_sleep(0.05)" # generate some i/o wait
+    City::connection.execute "select pg_sleep(0.02)" # generate some i/o wait
     @users = User.limit(20).all
     render action: 'index'
   end
